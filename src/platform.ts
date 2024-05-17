269c52987314c4453fc0c2ccf0ac870e0399f2af
Xander Posner <xander.posner@gmail.com>
2024-05-16 22:15:16 -0500

    latest

--- src/platform.ts
+++ src/platform.ts
@@ -1,122 +1,195 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
-
+import axios from 'axios';
+import { CronJob } from 'cron';
+import { DateTime } from 'luxon';
+import * as fs from 'fs';
+import * as path from 'path';
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
 
-/**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
- */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+const relevantZmanimKeys = [
+  'chatzotNight', 'misheyakir', 'dawn', 'sunrise', 'sofZmanShma', 'sofZmanTfilla',
+  'chatzot', 'minchaGedola', 'minchaKetana', 'plagHaMincha', 'sunset', 'beinHaShmashos', 'tzeit85deg',
+];
+
+interface MostRecentTime {
+  label: string;
+  time: DateTime;
+}
+
+export class ZmanimSwitches implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
-
-  // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
+  private recentTimeFile: string;
+  private zmanimUrl!: string;
+  private switchNames: Record<string, string>;
 
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
+    this.recentTimeFile = path.join(this.api.user.persistPath(), 'recent_time.txt');
+    this.switchNames = config.switchNames || {};
 
-    // Homebridge 1.8.0 introduced a `log.success` method that can be used to log success messages
-    // For users that are on a version prior to 1.8.0, we need a 'polyfill' for this method
-    if (!log.success) {
-      log.success = log.info;
+    if (!config.geonameid) {
+      this.log.error('Geoname ID is required for this plugin to function.');
+      return;
     }
 
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
+    this.zmanimUrl = `https://www.hebcal.com/zmanim?cfg=json&geonameid=${config.geonameid}`;
+
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      this.initializeAccessories();
+      this.scheduleZmanimFetch();
     });
   }
 
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to set up event handlers for characteristics and update respective values.
-   */
   configureAccessory(accessory: PlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
-
-    // add the restored accessory to the accessories cache, so we can track if it has already been registered
     this.accessories.push(accessory);
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
+  initializeAccessories() {
+    relevantZmanimKeys.forEach((key) => {
+      const uuidKey = this.api.hap.uuid.generate(`homebridge-zmanim.${key}`);
+      const accessory = new this.api.platformAccessory(this.switchNames[key] || key, uuidKey);
+      const service = accessory.addService(this.Service.Switch, this.switchNames[key] || key);
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+      service.getCharacteristic(this.Characteristic.On)
+        .on('get', (callback) => this.getSwitchState(key, callback))
+        .on('set', (value, callback) => this.setSwitchState(key, value, callback));
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      this.accessories.push(accessory);
+    });
 
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+    this.updateSwitchStates();
+  }
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+  scheduleZmanimFetch() {
+    new CronJob('0 2 * * *', () => this.fetchAndUpdateZmanim(), null, true, 'America/Chicago');
+    this.fetchAndUpdateZmanim(); // Fetch immediately on startup
+  }
 
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+  async fetchAndUpdateZmanim() {
+    try {
+      const response = await axios.get(this.zmanimUrl);
+      const zmanim = response.data.times;
+      this.log.info('Successfully fetched Zmanim.');
+      const now = DateTime.now().setZone('America/Chicago');
+      let mostRecentTime: MostRecentTime | null = null;
 
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      for (const key of relevantZmanimKeys) {
+        const zmanTime = DateTime.fromISO(zmanim[key]).setZone('America/Chicago');
+        if (zmanTime <= now) {
+          mostRecentTime = { label: key, time: zmanTime };
+        }
       }
+
+      if (mostRecentTime) {
+        fs.writeFileSync(this.recentTimeFile, mostRecentTime.label, 'utf8');
+        this.updateSwitchStates();
+      }
+    } catch (error) {
+      this.log.error('Error fetching Zmanim:', error);
+    }
+  }
+
+  updateSwitchStates() {
+    const mostRecentTime = fs.existsSync(this.recentTimeFile) ? fs.readFileSync(this.recentTimeFile, 'utf8') : null;
+
+    this.accessories.forEach((accessory) => {
+      const service = accessory.getService(this.Service.Switch);
+      if (service) {
+        const isOn = accessory.displayName === mostRecentTime;
+        service.updateCharacteristic(this.Characteristic.On, isOn);
+      }
+    });
+
+    this.applyLogicBasedOnMostRecentTime(mostRecentTime);
+  }
+
+  getSwitchState(key, callback) {
+    const mostRecentTime = fs.existsSync(this.recentTimeFile) ? fs.readFileSync(this.recentTimeFile, 'utf8') : null;
+    const isOn = key === mostRecentTime;
+    callback(null, isOn);
+  }
+
+  setSwitchState(key, value, callback) {
+    this.updateSwitchStates();
+    callback(null);
+  }
+
+  getRecentTime(): string | null {
+    return fs.existsSync(this.recentTimeFile) ? fs.readFileSync(this.recentTimeFile, 'utf8') : null;
+  }
+
+  applyLogicBasedOnMostRecentTime(mostRecentTime: string | null) {
+    const turnOff = (name: string) => {
+      const accessory = this.accessories.find(acc => acc.displayName === name);
+      if (accessory) {
+        const service = accessory.getService(this.Service.Switch);
+        service?.updateCharacteristic(this.Characteristic.On, false);
+      }
+    };
+
+    const turnOn = (name: string) => {
+      const accessory = this.accessories.find(acc => acc.displayName === name);
+      if (accessory) {
+        const service = accessory.getService(this.Service.Switch);
+        service?.updateCharacteristic(this.Characteristic.On, true);
+      }
+    };
+
+    if (mostRecentTime === 'chatzotNight') {
+      turnOn('chatzotNight');
+    } else if (mostRecentTime === 'misheyakir') {
+      turnOn('misheyakir');
+      turnOn('sofZmanShma');
+      turnOn('sofZmanTfilla');
+    } else if (mostRecentTime === 'dawn') {
+      turnOff('misheyakir');
+      turnOn('dawn');
+      turnOn('sofZmanShma');
+      turnOn('sofZmanTfilla');
+    } else if (mostRecentTime === 'sunrise') {
+      turnOff('dawn');
+      turnOn('sunrise');
+      turnOn('sofZmanShma');
+      turnOn('sofZmanTfilla');
+    } else if (mostRecentTime === 'sofZmanShma') {
+      turnOn('sunrise');
+      turnOff('sofZmanShma');
+      turnOn('sofZmanTfilla');
+    } else if (mostRecentTime === 'sofZmanTfilla') {
+      turnOn('sunrise');
+      turnOff('sofZmanShma');
+      turnOff('sofZmanTfilla');
+    } else if (mostRecentTime === 'chatzot') {
+      turnOn('chatzot');
+      turnOff('sunrise');
+    } else if (mostRecentTime === 'minchaGedola') {
+      turnOn('minchaGedola');
+      turnOn('chatzot');
+    } else if (mostRecentTime === 'minchaKetana') {
+      turnOn('minchaKetana');
+      turnOff('minchaGedola');
+      turnOn('chatzot');
+    } else if (mostRecentTime === 'plagHaMincha') {
+      turnOn('plagHaMincha');
+      turnOff('chatzot');
+      turnOff('minchaKetana');
+    } else if (mostRecentTime === 'sunset') {
+      turnOff('plagHaMincha');
+      turnOn('sunset');
+    } else if (mostRecentTime === 'beinHaShmashos') {
+      turnOff('sunrise');
+      turnOn('beinHaShmashos');
+    } else if (mostRecentTime === 'tzeit85deg') {
+      turnOff('beinHaShmashos');
+      turnOn('tzeit85deg');
     }
   }
 }
